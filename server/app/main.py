c92327bf38633a157db594a1b3b16db4aef53fea
jemaie <jerome.maier@uzh.ch>
2024-12-04 18:11:28 +0100

    adj fastAPI instance

--- server/app/main.py
+++ server/app/main.py
@@ -3,6 +3,9 @@ import os
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
+from models.message import Message
+from models.response import Response
+
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 app = FastAPI()
@@ -18,3 +21,9 @@ app.add_middleware(
 @app.get("/health")
 async def health_check():
     return {"status": "healthy"}
+
+
+@app.post("/message")
+async def post_message(message: Message):
+    print(message)
+    return Response(message="test", actions="test")
